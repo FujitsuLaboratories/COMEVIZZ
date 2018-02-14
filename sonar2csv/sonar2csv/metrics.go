d6731b74242ea68e72758b10f646fcb88e8b9438
Yusuke Nemoto <y-nemoto@jp.fujitsu.com>
2018-02-15 08:53:48 +0900

    Fix endpoint for SonarQube 6.3 or later (#17)
    * Fix endpoint for SonarQube 6.3 or later
    
    * fix for SonarQube6.7
    
    * update config file for SonarQube 6.7
    
    * update SonarResponse struct

--- sonar2csv/sonar2csv/metrics.go
+++ sonar2csv/sonar2csv/metrics.go
@@ -3,37 +3,34 @@ package sonar2csv
 import (
 	"log"
 	"os"
-	"strconv"
 
 	"github.com/FujitsuLaboratories/COMEVIZZ/sonar2csv/util"
 )
 
 // sonarResponse は Sonar WebAPI - /api/resource のレスポンスのモデル
 type SonarResponse struct {
-	ID           int       `json:"id"`
-	UUID         string    `json:"uuid"`
-	Key          string    `json:"key"`
-	Name         string    `json:"name"`
-	Scope        string    `json:"scope"`
-	Qualifier    string    `json:"qualifier"`
-	Date         string    `json:"date"`
-	CreationDate string    `json:"creationDate"`
-	Lname        string    `json:"lname"`
-	Version      string    `json:"version"`
-	Msr          []Metrics `json:"msr"`
+	Component    Component `json:"component"`
 }
 
-// Metrics はSonar WebAPI - /api/resourceのレスポンスのうち、メトリクスに関するモデル
+type Component struct {
+	ID           string    `json:"id"`
+	Key          string    `json:"key"`
+	Name         string    `json:"name"`
+	Qualifier    string    `json:"qualifier"`
+	Branch       string    `json:"branch"`
+	Msr          []Metrics `json:"measures"`
+}
+
+// Metrics はSonar WebAPI - /api/measures/componentのレスポンスのうち、メトリクスに関するモデル
 type Metrics struct {
-	Key     string  `json:"key"`
-	Val     float64 `json:"val"`
-	Frmtval string  `json:"frmt_val"`
+	Key     string  `json:"metric"`
+	Val     string  `json:"value"`
 }
 
 func (s *SonarResponse) MergeMetrics(originFile string, appendix map[string]string) ([]map[string]string, error) {
 	vals := map[string]string{}
-	for _, v := range s.Msr {
-		vals[v.Key] = strconv.FormatFloat(v.Val, 'f', -2, 64)
+	for _, v := range s.Component.Msr {
+		vals[v.Key] = v.Val
 	}
 	for k, v := range appendix {
 		vals[k] = v
